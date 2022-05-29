65d78c36144fd8a7773b96c400a17d8c64c29fd3
Damla Metin <damla91@gmail.com>
2022-05-29 21:57:05 +0300

    created board

---
+++ StarloopBoardGame/Assets/Scripts/BoardController.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardController : MonoBehaviour
+{
+
+    public GameObject tilePrefab;
+    public GameObject tilePrefabParent;
+
+    public float tileOffset = 1.2f;
+}
